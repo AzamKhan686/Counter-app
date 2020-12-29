6237ac501ec3753b4d121c441c0bfe780218b3a2
AzamKhan686 <umerk7222@gmail.com>
2020-12-29 23:09:45 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+//import logo from './logo.svg';
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+import "./App.css";
+
+/*class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <main className="container">
+          <Counters />
+        </main>
+      </React.Fragment>
+    );
+  }
+}*
+
+export default App;*/
+function App() {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Counters />
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
